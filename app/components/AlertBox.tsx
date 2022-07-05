c8b67a5000c9c62fbcac2a7fe599f9fdbac45565
Abhishek Bijalwan <abhishek.bijalwan092@gmail.com>
2022-07-05 23:51:58 +0530

    Update alertbox component

--- app/components/AlertBox.tsx
+++ app/components/AlertBox.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-
-const AlertBox = ({ alertMessage }) => (
+const AlertBox = ({ alertMessage } : { alertMessage: string}) => (
   <div
     className="mt-5 p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800 m-2 sm:mx-0"
     role="alert"
