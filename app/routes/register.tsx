551701bc9f4f34ec9ef370e637a4c61c9463391a
Abhishek Bijalwan <abhishek.bijalwan092@gmail.com>
2022-07-08 00:43:02 +0530

    add actions and setup api calls for register route

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,64 +1,131 @@
-import { useState } from 'react'
-import { Link } from "@remix-run/react"
+import { useActionData, Link, useTransition } from '@remix-run/react'
+import type { ActionFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
 
-import * as Yup from 'yup'
-import { Formik, Form } from 'formik'
+import { validateEmail, validatePassword, validateName } from '~/utils/validation'
+import { register } from '~/utils/session.server'
+import { db } from '~/utils/db.server'
 
 import Button from '../components/Button'
 import Input from '../components/Input'
 import AlertBox from '../components/AlertBox'
 import FormHeader from '../components/FormHeader'
 
-const Register = () => {
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  const handleRegister = () => {
-
+type ActionData = {
+  formError?: string
+  fieldErrors?: {
+    email: string | undefined
+    password: string | undefined
+    firstName: string | undefined
+    lastName: string | undefined
   }
+  fields?: {
+    email: string
+    password: string
+    firstName: string
+    lastName: string
+  }
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 })
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+
+  const firstName = form.get('firstname')
+  const lastName = form.get('lastname')
+  const email = form.get('email')
+  const password = form.get('password')
+
+  if (
+    typeof firstName !== 'string' ||
+    typeof lastName !== 'string' ||
+    typeof email !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return badRequest({
+      formError: `Form not submitted correctly.`
+    })
+  }
+
+  const fields = { firstName, lastName, email, password }
+  const fieldErrors = {
+    firstName: validateName(firstName, 'firstName'),
+    lastName: validateName(lastName, 'lastName'),
+    email: validateEmail(email),
+    password: validatePassword(password)
+  }
+
+  if (Object.values(fieldErrors).some(Boolean)) return badRequest({ fieldErrors, fields })
+
+  const existingUser = await db.user.findFirst({ where: { email } })
+
+  if (existingUser) {
+    return {
+      fields,
+      formError: `User with email ${email} already exists`
+    }
+  }
+
+  const user = await register({ firstName, lastName, email, password })
+
+  if (!user) {
+    return {
+      fields,
+      formError: `Something went wrong trying to create a new user.`
+    }
+  }
+
+  return 'Registration successfull'
+}
+
+const Register = () => {
+  const actionData = useActionData()
 
   return (
     <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <FormHeader>Register for an account</FormHeader>
-      {error && (
+      {actionData?.formError && (
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <AlertBox alertMessage={error} />
+          <AlertBox alertMessage={actionData?.formError} />
         </div>
       )}
-      <div className="sm:mx-auto sm:w-full sm:max-w-md rounded bg-white shadow shadow-md">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md rounded bg-white shadow-md">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="py-8 px-4 sm:px-10">
-            <Formik
-              initialValues={{
-                firstName: '',
-                lastName: '',
-                email: '',
-                password: ''
-              }}
-              validationSchema={Yup.object({
-                firstName: Yup.string().required('First Name is required'),
-                lastName: Yup.string().required('Last Name is required'),
-                email: Yup.string().email('Invalid email address').required('Email is required'),
-                password: Yup.string().required('Password is required')
-              })}
-              onSubmit={() => {
-                handleRegister()
-              }}
-            >
-              <Form>
-                <Input label="First Name" type="text" name="firstName" />
-                <Input label="Last Name" type="text" name="lastName" />
-                <Input label="Email" type="email" name="email" />
-                <Input label="Password" type="password" name="password" />
+            <form method="post">
+              <Input
+                label="First Name"
+                type="text"
+                name="firstname"
+                error={actionData?.fieldErrors?.firstName}
+              />
+              <Input
+                label="Last Name"
+                type="text"
+                name="lastname"
+                error={actionData?.fieldErrors?.lastName}
+              />
+              <Input
+                label="Email"
+                type="email"
+                name="email"
+                error={actionData?.fieldErrors?.email}
+              />
+              <Input
+                label="Password"
+                type="password"
+                name="password"
+                error={actionData?.fieldErrors?.password}
+              />
 
-                <div className="mt-7">
-                  <Button type="submit" label="Register" loading={loading} message="Signing up" />
-                  <Link to="/">
-                    <Button label="Login" variant="outlined" loading={false} classes="mt-3" />
-                  </Link>
-                </div>
-              </Form>
-            </Formik>
+              <div className="mt-7">
+                <Button type="submit" label="Register" message="Signing up" />
+                <Link to="/login">
+                  <Button label="Login" variant="outlined" loading={false} classes="mt-3" />
+                </Link>
+              </div>
+            </form>
           </div>
         </div>
       </div>
