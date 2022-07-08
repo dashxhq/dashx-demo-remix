4029c8b22026b509fcb6d0095c3544ccd6008aed
Abhishek Bijalwan <abhishek.bijalwan092@gmail.com>
2022-07-08 22:03:48 +0530

    setup session cookies

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,5 +1,22 @@
+import { createCookieSessionStorage, redirect } from '@remix-run/node'
+import type { SessionStorage } from '@remix-run/node'
+
 import bcrypt from 'bcrypt'
 import { db } from './db.server'
+
+const sessionSecret = process.env.SESSION_SECRET || ''
+
+const storage: SessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: 'Dashx_demo',
+    secure: true,
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true
+  }
+})
 
 type RegisterType = {
   firstName: string
@@ -13,7 +30,7 @@ type LoginType = {
 }
 
 export async function register({ firstName, lastName, email, password }: RegisterType) {
-  const passwordHash = await bcrypt.hash(password, 10)
+  const passwordHash: string = await bcrypt.hash(password, 10)
 
   const user = await db.user.create({
     data: { firstName, lastName, email, passwordHash }
@@ -52,4 +69,20 @@ export async function resetPassword({ email, password }: LoginType) {
   await db.user.update({ where: { email }, data: { passwordHash } })
 
   return null
+}
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession()
+
+  if (!sessionSecret) {
+    throw new Error('Must enviornment variable SESSION_SECRET')
+  }
+
+  session.set('userId', userId)
+
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session)
+    }
+  })
 }
