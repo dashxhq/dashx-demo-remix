b7fe37926a452ded21c3e628b1e1b54636cd8c4e
Abhishek Bijalwan <abhishek.bijalwan092@gmail.com>
2022-07-21 19:08:08 +0530

    Create contact us API route

--- app/routes/contact-us.tsx
+++ app/routes/contact-us.tsx
@@ -1,4 +1,5 @@
-import { Link } from "@remix-run/react"
+import { Form, Link, useActionData, useTransition } from '@remix-run/react'
+import { ActionFunction, json } from '@remix-run/node'
 
 import AlertBox from '../components/AlertBox'
 import Button from '../components/Button'
@@ -6,36 +7,99 @@ import FormHeader from '../components/FormHeader'
 import Input from '../components/Input'
 import TextArea from '../components/TextArea'
 import SuccessBox from '../components/SuccessBox'
+import { validateEmail, validateName } from '~/utils/validation'
+import { contactUs } from '~/utils/session.server'
+
+type ActionData = {
+  formError?: string
+  fieldErrors?: {
+    email: string | undefined
+    password?: string | undefined
+    name?: string | undefined
+    feedback?: string | undefined
+    firstName?: string | undefined
+    lastName?: string | undefined
+  }
+  fields?: {
+    name?: string
+    email: string
+    password?: string
+    firstName?: string
+    lastName?: string
+    feedback?: string
+  }
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 })
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+
+  const name = form.get('name')
+  const email = form.get('email')
+  const feedback = form.get('feedback')
+
+  if (typeof name !== 'string' || typeof email !== 'string' || typeof feedback !== 'string') {
+    return badRequest({
+      formError: `Form not submitted correctly.`
+    })
+  }
+
+  const fields = { name, email, feedback }
+  const fieldErrors = {
+    name: validateName(name, 'Name'),
+    email: validateEmail(email),
+    feedback: validateName(feedback, 'Feedback')
+  }
+
+  if (Object.values(fieldErrors).some(Boolean)) return badRequest({ fieldErrors, fields })
+
+  return contactUs({ name, email, feedback })
+}
 
 const Contact = () => {
-  let error, successMessage
-  
+  const actionData = useActionData()
+  const { state } = useTransition()
+
   return (
-    <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8 h-screen font-poppins bg-gray-50">
       <FormHeader>Contact Us</FormHeader>
-      {error && (
+      {actionData?.formError && (
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <AlertBox alertMessage={error} />
+          <AlertBox alertMessage={actionData?.formError} />
         </div>
       )}
       <div className="sm:mx-auto sm:w-full sm:max-w-md rounded bg-white shadow shadow-md pt-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md pb-8">
-          {successMessage && (
+          {actionData?.success ? (
             <div className="sm:mx-auto sm:w-full sm:max-w-md text-center py-4 pt-1 px-4 sm:px-6">
-              <SuccessBox successMessage={successMessage} />
+              <SuccessBox successMessage={actionData?.success} />
             </div>
-          )}
-          {!successMessage && (
+          ) : (
             <div className="py-8 pt-1 px-4 sm:px-10">
-                <form>
-                  <Input label="Name" type="text" name="name" />
-                  <Input label="Email" type="email" name="email" />
-                  <TextArea label="Send us a message" name="feedback" />
+              <Form method="post">
+                <Input label="Name" type="text" name="name" error={actionData?.fieldErrors?.name} />
+                <Input
+                  label="Email"
+                  type="email"
+                  name="email"
+                  error={actionData?.fieldErrors?.email}
+                />
+                <TextArea
+                  label="Send us a message"
+                  name="feedback"
+                  error={actionData?.fieldErrors?.feedback}
+                />
 
-                  <div className="mt-7">
-                    <Button type="submit" label="Submit" />
-                  </div>
-                </form>
+                <div className="mt-7">
+                  <Button
+                    type="submit"
+                    label="Submit"
+                    loading={state === 'submitting'}
+                    message=""
+                  />
+                </div>
+              </Form>
             </div>
           )}
           <div className="text-sm text-center">
